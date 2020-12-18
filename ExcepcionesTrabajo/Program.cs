98e5de31ab08284e5d5659834fa335ed54612f8f
brayan593 <baa.andrade@yavirac.edu.ec>
2020-12-17 23:30:10 -0600

    poner cualquier nombre

---
+++ ExcepcionesTrabajo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ExcepcionesTrabajo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Jugar cancha = new Jugar() { Nombre = "Michael", NumeroDeJugadores = 1 };
+                cancha.NumeroDeJugadores = 11;
+                if (cancha.NumeroDeJugadores >= 10)
+                {
+                    throw new DiversionException("Si esta cansado");
+                }
+            }
+            catch (DiversionException)
+            {
+                Console.WriteLine("Sientate y descansa");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("El jugador salio de la cancha");
+            }
+        }
+        public class DiversionException : Exception
+        {
+            public DiversionException(string message): base(message)
+            {
+
+            }
+        }
+        public class Jugar
+        {
+            public string Nombre { get; set; }
+            public int NumeroDeJugadores { get; set; }
+        }
+    }
+}
